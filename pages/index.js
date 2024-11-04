4804934b52d5eb55784fa656b4bbd2aebc74f210
Gustavo Ruschel <157659398+GustavoRuschel@users.noreply.github.com>
2024-11-04 17:55:34 +0000

    Alterando o tempo do timer

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,23 @@
 import { useEffect, useState } from 'react';
 
 function Home() {
-    const [text, setText] = useState("Pensa em um número de 1 a 10");
+    const [text, setText] = useState("Pensa em um número de 1");
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setText("Vai estudar e para de perder tempo! 😹");
-        }, 5000); // muda o texto após 5 segundos
+        }, 2000); // muda o texto após 5 segundos
 
         return () => clearTimeout(timer); // limpa o timer se o componente for desmontado
     }, []);
 
     return (
-        <div style={{ textAlign: "center", padding: "20px" }}>
+        <div style={{ textAlign: "center", padding: "40px" }}>
             <h1>{text}</h1>
         </div>
     );
 }
 
 export default Home;
+
+
